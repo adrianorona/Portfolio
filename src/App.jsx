58c78c1b5b8ba404f7e86f70b8a5424227e3ec3d
leeadriannorona <leeadrian.norona@cvsu.edu.ph>
2026-02-02 15:23:14 +0800

    Fix CSS and remove Nebula component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,11 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Achievements from './components/Achievements'
 import Contact from './components/Contact'
-import Nebula from './components/Nebula'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      {/* Nebula background - deepest layer */}
-      <Nebula />
-      
       <Navbar />
       
       {/* Globe as hero background */}
